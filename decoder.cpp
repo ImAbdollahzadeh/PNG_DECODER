393e591dbf455f73a1b7fde0899e1d41841524af
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2018-10-30 16:55:07 +0100

    Update decoder.cpp

--- decoder.cpp
+++ decoder.cpp
@@ -11,6 +11,15 @@
 #define BUTTON_ID        0x321
 #define TEXT_ID          0x322
 
+wchar_t* convert_char_to_wchar(const char *c) {
+	const size_t cSize = strlen(c) + 1;
+	wchar_t* wc = new wchar_t[cSize];
+	mbstowcs(wc, c, cSize);
+	return wc;
+}
+
+#define L(STRING) convert_char_to_wchar(STRING)
+
 typedef unsigned char  BYTE;
 typedef unsigned short WORD;
 typedef unsigned int   DOUBLEWORD;
